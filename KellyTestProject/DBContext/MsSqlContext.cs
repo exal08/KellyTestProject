fb76d1af5d9a2120d091b1131fdcc3819cb4db33
Alexandr <exal08@mail.ru>
2020-12-01 00:07:24 +0500

    Создание контекста для работы с БД

---
+++ KellyTestProject/DBContext/MsSqlContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using KellyTestProject.Model;
+
+namespace KellyTestProject.DBContext
+{
+    public class MsSqlContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Section> Sections { get; set; }
+        public DbSet<Visit> Visits { get; set; }
+        public MsSqlContext(DbContextOptions<MsSqlContext> options) : base(options) { }
+    }
+}
--- KellyTestProject/Startup.cs
+++ KellyTestProject/Startup.cs
@@ -1,7 +1,8 @@
+using KellyTestProject.DBContext;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -20,6 +21,9 @@ namespace KellyTestProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MsSqlContext>(options =>
+               options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")
+               ));
 
             services.AddControllersWithViews();
 
