291805724eb9d36aa715c185fddbb310560af4d7
Alexandr <exal08@mail.ru>
2020-11-30 23:58:41 +0500

    Описал модель данных

---
+++ KellyTestProject/Model/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KellyTestProject.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Patronymic { get; set; }
+        public List<Visit> Visits { get; set; }
+    }
+}
---
+++ KellyTestProject/Model/Visit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KellyTestProject.Model
+{
+    public class Visit
+    {
+        public int Id { get; set; }
+        public DateTime DateTimeVisit { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int SectionId { get; set; }
+        public Section Section { get; set; }
+    }
+}
