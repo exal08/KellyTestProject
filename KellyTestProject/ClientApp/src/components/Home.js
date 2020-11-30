1012cbcc2dbe33f3c4e304e8ef8dc22b72bfd757
Alexandr <exal08@mail.ru>
2020-12-01 00:38:32 +0500

    Вывод списка разделов

--- KellyTestProject/ClientApp/src/components/Home.js
+++ KellyTestProject/ClientApp/src/components/Home.js
@@ -1,13 +1,52 @@
 import React, { Component } from 'react';
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = 'Разделы';
 
-  render () {
-    return (
-      <div>
-      
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = { Sections: [], loading: true };
+    }
+
+    componentDidMount() {
+        this.loadSections();
+    }
+
+    static renderSectionsTable(Sections) {
+        return (
+            <table className='table table-striped' aria-labelledby="tabelLabel">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Название раздела</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    {Sections.map(forecast =>
+                        <tr key={forecast.id}>
+                            <td>{forecast.id}</td>
+                            <td>{forecast.name}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+    }
+
+    render() {
+        let content = this.state.loading ? <p>Загрузка...</p> : Home.renderSectionsTable(this.state.Sections);
+        return (
+            <div>
+                <h1>Список разделов</h1>
+                { content }
+            </div>
+        );
+    }
+
+    async loadSections() {
+        const response = await fetch('api/Section');
+        const data = await response.json();
+        this.setState({ Sections: data, loading: false });
+    }
 }
---
+++ KellyTestProject/Controllers/SectionController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using KellyTestProject.DBContext;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using KellyTestProject.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace KellyTestProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SectionController : ControllerBase
+    {
+        private readonly MsSqlContext dbContext;
+        public SectionController(MsSqlContext context)
+        {
+            dbContext = context;
+        }
+
+        [HttpGet]
+        public async Task<List<Section>> Get()
+        {
+            return await dbContext.Sections.ToListAsync();
+        }
+    }
+}
